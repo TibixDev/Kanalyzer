05e69b81c0ff66078b18514b36c49990f8963ae3
Tibix <fuloptibi03@gmail.com>
2021-08-17 14:34:36 +0300

    Added README.md, updated About.vue

---
+++ README.md
@@ -0,0 +1,26 @@
+# Kanalyzer
+
+## Description
+Kanalyzer is a free and open source webapp made using Vue.js that helps you learn kana.
+## Features
+- Learn Hiragana and Katakana (Dakuten & Combination WIP)
+- Get statistics for completed quizes
+- Select kana groups for quizes to your liking
+
+## Production Build
+[Here](https://kanalyzer.netlify.app/)
+
+## Project setup
+```
+npm i
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container text-lg mt-5 mx-auto px-4">
-        <h1 class="text-7xl text-center mb-12">Kanalyzer</h1>
+    <div class="container text-xl mt-5 mx-auto px-4 text-center">
+        <h1 class="text-7xl mb-5">Kanalyzer</h1>
         <p><strong>KanaLyzer</strong> is a free and open source webapp which helps you learn Hiragana and Katakana.</p>
-        <p>Repository: <a href="#" class="text-purple-400">GitHub</a></p>
+        <p>Repository: <a href="https://github.com/TibixDev/kanalyzer" class="text-purple-400">GitHub</a></p>
         <p>Copyright (c) 2021 TibixDev</p>
     </div>
 </template>
